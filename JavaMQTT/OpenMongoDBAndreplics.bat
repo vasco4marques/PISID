aac2b46c048313653adf635e263dbb93b60318c5
Vasco Marques <vasco4marques@gmail.com>
2024-04-27 18:10:08 +0100

    Retirar os bat files

--- JavaMQTT/OpenMongoDBAndreplics.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-start "Server S1" /MIN mongod --config c:\replica\imdb_S1\imdb_S1.conf
-start "Server S2" /MIN mongod --config c:\replica\imdb_S2\imdb_S2.conf
-start "Server S3" /MIN mongod --config c:\replica\imdb_S3\imdb_S3.conf
